a8bb67c116f21cb57aceb7e5aaab7eeb0fb9fe51
Gokulnath V <93648787+Coding-Devil@users.noreply.github.com>
2024-02-15 22:08:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     .then(data => {
         console.log('Success:', data);
         // Redirect to Google Form
-        window.location.href = 'https://forms.gle/f8gWPmVBUTcSgoHLA';
+        window.location.href = 'next.html';
     })
     .catch((error) => {
         console.error('Error:', error);
